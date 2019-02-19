8aa58a38d90f3c0a7fa15bba91422ec4f7d3110b
caiqueaugustocm <44869122+caiqueaugustocm@users.noreply.github.com>
2019-02-19 16:12:36 -0300

    Adicionando coisas novas
    Classes adicionais para o RPG

---
+++ Itens/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chaos_Divinity.Itens
+{
+    class Item
+    {
+        private string nome { get; } //nome do item/equipamento/consumívle
+        private string classes { get; } //qual profissão poder usar o item
+        private string tipo { get; } //equipameno, poção, comida, texto, outros     
+        private string categoria { get; } //normal, raro, lendário
+        private Status atributos { get; set; } //mudar ou pegar status
+    }
+}
---
+++ Itens/Status.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chaos_Divinity.Itens
+{
+    class Status
+    {
+        private int str { get; }//strength
+        private int agi { get; }//agility
+        private int vit { get; }//vitality
+        private int wis { get; }//wisdom -> inteligência
+        private int luk { get; }//luck
+        private int end { get; }//endurance
+    }
+}
